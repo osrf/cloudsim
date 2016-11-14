02ed8bf732931d257072f1cc5b053129062c3cae
chapulina <burajiru.no.chapulina@gmail.com>
2016-11-14 13:30:38 -0800

    cofigurable, don't clone old components

--- setup.bash
+++ setup.bash
@@ -10,5 +10,13 @@ cd $DIR
 echo -e "\e[0;33mCloning / updating repositories...\033[0m"
 vcs import < repositories
 
+echo -e "\e[0;33mBuilding repositories...\033[0m"
 $DIR/build.bash
+
+if [ $# == 1 ] && [ "$1" == link ]; then
+  echo -e "\e[0;33mLinking repositories...\033[0m"
+  $DIR/build.bash
+else
+  echo -e "\e[0;33mNot linking repositories, you can run [link.bash] manually.\033[0m"
+fi
 
